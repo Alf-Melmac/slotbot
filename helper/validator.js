e6bf4d566292977eb4f116a8d5ec56a602703075
Alf <62615304+Alf-Melmac@users.noreply.github.com>
2020-09-10 23:55:06 +0200

    Added option to check if number is a discordUserId

--- helper/validator.js
+++ helper/validator.js
@@ -6,6 +6,11 @@ class Validator {
     static time(time) {
         return !time.match('^([0-2][0-3]|[0-1][0-9]):[0-5][0-9]+$');
     }
+
+    static isUser(id) {
+        //For 18 characters we can assume that it is a Discord User ID
+        return Math.ceil(Math.log10(id + 1)) === 16;
+    }
 }
 
 module.exports = Validator;
